1fc3c734e328705f5bfa0edf60de166ed76e0741
Beth <beth.nieters@gmail.com>
2018-03-26 18:43:25 -0500

    First commit

---
+++ app/src/main/java/com/example/bethn/gfree_srproj/Restaurant.java
@@ -0,0 +1,38 @@
+package com.example.bethn.gfree_srproj;
+
+/**
+ * Created by bethn on 3/25/2018.
+ */
+
+public class Restaurant {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Restaurant() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/com/example/bethn/gfree_srproj/addRestaurant.java
@@ -0,0 +1,13 @@
+package com.example.bethn.gfree_srproj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurant);
+    }
+}
